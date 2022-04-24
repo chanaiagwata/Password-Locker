9c4ac384db23a25ff59545918e669bc0f81340f9
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-04-24 14:19:05 +0300

    feat:will authenticate password

--- run.py
+++ run.py
@@ -18,10 +18,17 @@ def main():
             print("username...")
             user_name_created = input()
             
-            print("Password...")
+            print("password...")
             user_password_created = input()
             
-            print("Confirm Password...")
+            print("confirm Password...")
             user_password_confirmed = input()
             
-            
+            while user_password_created != user_password_confirmed:  #To authenticate password
+                print("password entered doesn't belong to the account! check and try again")
+                print("enter password")
+                user_password_created = input()
+                print("confirm password")
+                user_password_confirmed = input ()
+            else:
+                print(f"impressive!, new account {user_name_created} created successfuly")
